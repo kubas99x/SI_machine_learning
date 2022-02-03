de8b2e05c43e707c8d14b5e626eb9d3f72878678
kubas99x <kubarychel@gmail.com>
2022-02-03 15:43:33 +0100

    Dodanie dodatkowej maski koloru czerwonego.

--- main.py
+++ main.py
@@ -25,39 +25,32 @@ import cv2
 
 # load the image
 path=[]
-for i in range(7):
+for i in range(8):
     path.append(f"D:\programy_SI\PROJEKT_SI\znak_{i}_test.png")
 
 #image = cv2.imread("D:\programy_SI\PROJEKT_SI\znak_1_test.png")
 
 for path_ in path:
     image = cv2.imread(path_)
-    mask = cv2.inRange(image, np.array([17, 15, 100]), np.array([50, 56, 200]))
-    output = cv2.bitwise_and(image, image, mask=mask)
+    # mask = cv2.inRange(image, np.array([17, 15, 100]), np.array([50, 56, 200]))
+    # output = cv2.bitwise_and(image, image, mask=mask)
 
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    mask2 = cv2.inRange(hsv, np.array([160, 150, 80]), np.array([180, 255, 255]))
+    mask_hsv_lower = cv2.inRange(hsv, np.array([0, 120, 10]), np.array([10, 255, 255]))
+    mask_hsv_upper = cv2.inRange(hsv, np.array([160, 80, 10]), np.array([180, 255, 255]))
+    mask2 = mask_hsv_upper + mask_hsv_lower
     output_hsv = cv2.bitwise_and(hsv, hsv, mask=mask2)
-
-    output_circle_2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    circles2 = cv2.HoughCircles(output_circle_2, cv2.HOUGH_GRADIENT, 1, 100,
-                               param1=50, param2=25, minRadius=0, maxRadius=0)
 
     # powrot z hsv do RGB i z RGB do szarosci
     image_RGB = cv2.cvtColor(output_hsv, cv2.COLOR_HSV2BGR)
     image_HSV_RGB_GREY = cv2.cvtColor(image_RGB, cv2.COLOR_BGR2GRAY)
+    #dobre parametry kółek
+    # circles3 = cv2.HoughCircles(image_HSV_RGB_GREY, cv2.HOUGH_GRADIENT, 1, 100,
+    #                            param1=50, param2=25, minRadius=0, maxRadius=0)
+    #testowe parametry kółek
     circles3 = cv2.HoughCircles(image_HSV_RGB_GREY, cv2.HOUGH_GRADIENT, 1, 100,
                                param1=50, param2=25, minRadius=0, maxRadius=0)
 
-    try:
-        circles = np.uint16(np.around(circles2))
-        for i in circles[0, :]:
-            # draw the outer circle
-            cv2.circle(output_circle_2, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            # draw the center of the circle
-            cv2.circle(output_circle_2, (i[0], i[1]), 2, (0, 0, 255), 3)
-    except:
-        print('Problem z kółkami na RGB')
 
     # dla próby malowanie kółek na image
     try:
@@ -71,7 +64,6 @@ for path_ in path:
         print('Problem z kółkami szarosci')
 
     #cv2.imshow("maska",mask2)
-    cv2.imshow("images", np.hstack([image, output, output_hsv]))
-    cv2.imshow("circles in red", output_circle_2)
+    cv2.imshow(f"images{path_}", np.hstack([image, hsv, output_hsv]))
     cv2.waitKey(0)
 
--- notes_and_tests.py
+++ notes_and_tests.py
@@ -1,9 +1,10 @@
 import numpy as np
 import cv2
 import copy
+import colorsys
 
 # Wycinanie i robienie ramki
-image = cv2.imread("D:\programy_SI\PROJEKT_SI\znak_1_test.png")
+#image = cv2.imread("D:\programy_SI\PROJEKT_SI\znak_1_test.png")
 # print(image.shape)
 # output = copy.deepcopy(image)
 # ball = output[220:300, 120:220]
@@ -30,17 +31,54 @@ image = cv2.imread("D:\programy_SI\PROJEKT_SI\znak_1_test.png")
 # print(hsv_red)
 
 
-image = cv2.medianBlur(image, 5)
-#cimg = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1, 100,
-                           param1=50, param2=30, minRadius=0, maxRadius=0)
-circles = np.uint16(np.around(circles))
-for i in circles[0, :]:
-    # draw the outer circle
-    cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
-    # draw the center of the circle
-    cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
-cv2.imshow('detected circles', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# image = cv2.medianBlur(image, 5)
+# #cimg = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+# image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, 1, 100,
+#                            param1=50, param2=30, minRadius=0, maxRadius=0)
+# circles = np.uint16(np.around(circles))
+# for i in circles[0, :]:
+#     # draw the outer circle
+#     cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
+#     # draw the center of the circle
+#     cv2.circle(image, (i[0], i[1]), 2, (0, 0, 255), 3)
+# cv2.imshow('detected circles', image)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+print(colorsys.rgb_to_hsv(168, 18, 33))
+
+#parametry w miare dobre:
+#[160, 150, 80]), np.array([180, 255, 255])
+# H S V
+# H jest od 0 do 180
+# S od 0 do 255
+# V zostaw od 10 do 255
+#ostatni range nie zmieniaj, zostaw od 20 do 255
+# H z Gimpa dziel na pół! Bo tam jest zakres 360, w pythonie 180
+# S x2.5 z gimpa
+#W miarę dobrze:
+# np.array([160, 150, 10]), np.array([180, 255, 255])
+
+# np.array([160, 80, 10]), np.array([180, 255, 255])
+# path=[]
+# for i in range(7):
+#     path.append(f"D:\programy_SI\PROJEKT_SI\znak_{i}_test.png")
+
+#testy
+path=[]
+for i in range(100):
+    path.append(f"D:\programy_SI\PROJEKT_SI\images\/road{i}.png")
+
+for path_ in path:
+    image = cv2.imread(path_)
+    mask = cv2.inRange(image, np.array([17, 15, 100]), np.array([50, 56, 200]))
+    output = cv2.bitwise_and(image, image, mask=mask)
+
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    mask2 = cv2.inRange(hsv, np.array([160, 80, 10]), np.array([180, 255, 255]))
+    output_hsv = cv2.bitwise_and(hsv, hsv, mask=mask2)
+
+    cv2.imshow("images", np.hstack([image, output, hsv, output_hsv]))
+    cv2.waitKey(0)
