3b420bd93b498b9a9bc22b052f798e09187b49aa
kubas99x <kubarychel@gmail.com>
2022-02-11 18:49:33 +0100

    nothing is working right...

--- main.py
+++ main.py
@@ -100,15 +100,17 @@ def extract(imageDictionary):
     bow.setVocabulary(dictionary)
     for part in imageDictionary:
         image = cv2.imread(f'{part["path"]}\/images\/{part["fileName"]}')
-        for object in part["partDictionaries"]:
-            bndbox = object["bndboxDict"]
-            sightPart = image[bndbox["ymin"]:bndbox["ymax"], bndbox["xmin"]:bndbox["xmax"]]
-            grey = cv2.cvtColor(sightPart, cv2.COLOR_BGR2GRAY)
-            desc = bow.compute(grey, sift.detect(grey))  # input KeypointDescriptor, output imgDescriptor
-            if desc is not None:
-                object.update({'desc': desc})
-            else:
-                object.update({'desc': np.zeros((1, len(dictionary)))})
+        if part["partDictionaries"]:                                                    #it could be when we are taking parts from circles
+            for object in part["partDictionaries"]:
+                bndbox = object["bndboxDict"]
+                sightPart = image[bndbox["ymin"]:bndbox["ymax"], bndbox["xmin"]:bndbox["xmax"]]
+                grey = cv2.cvtColor(sightPart, cv2.COLOR_BGR2GRAY)
+                desc = bow.compute(grey, sift.detect(grey))  # input KeypointDescriptor, output imgDescriptor
+                if desc is not None:
+                    object.update({'desc': desc})
+                else:
+                    object.update({'desc': np.zeros((1, len(dictionary)))})
+
     return imageDictionary
 
 
@@ -130,7 +132,7 @@ def predictImage(rf, data):
             object.update({"predictedStatus": rf.predict(object['desc'])})
 
 
-def accuracyCalculate(dataTest):
+def accuracyCalculate(dataTest):        #only with test images using xml
     trueStatus = []
     predictedStatus = []
     for data in dataTest:
@@ -143,12 +145,28 @@ def accuracyCalculate(dataTest):
 def printingTestImages(dataDict):
     for data in dataDict:
         image = cv2.imread(f'{data["path"]}\/images\/{data["fileName"]}')
-        bndbox = data["partDictionaries"]["bndboxDict"]
-        sightPart = image[bndbox["ymin"]:bndbox["ymax"], bndbox["xmin"]:bndbox["xmax"]]
-        cv2.imshow(f"part", sightPart)
+        # bndbox = data["partDictionaries"]["bndboxDict"]
+        # sightPart = image[bndbox["ymin"]:bndbox["ymax"], bndbox["xmin"]:bndbox["xmax"]]
+        cv2.imshow(f'{data["path"]}\/images\/{data["fileName"]}', image)
+        i = 0
+        for tmp in data["partDictionaries"]:
+            print(f'Znak: {i}',tmp["status"], tmp["predictedStatus"])
+            i+=1
         cv2.waitKey(0)
 
-
+def printingChoosenparts(dataTest):
+    for data in dataTest:
+        image = cv2.imread(f'{data["path"]}\/images\/{data["fileName"]}')
+        #imageArray = None
+        #imageArray = np.hstack([image])
+        for part in data["partDictionaries"]:
+            bndbox = part["bndboxDict"]
+            sightPart = image[bndbox["ymin"]:bndbox["ymax"], bndbox["xmin"]:bndbox["xmax"]]
+            #imageArray = np.hstack([imageArray, sightPart])
+            cv2.imshow(f"{part['predictedStatus']}", np.hstack(image, sightPart))
+            cv2.waitKey(0)
+        # cv2.imshow(f"images", imageArray)
+        # cv2.waitKey(0)
 def makingMaskForCircles(hsvImage, lowerMaskL, lowerMaskH, higherMaskL, higherMaskH):
     lowerMask = cv2.inRange(hsvImage, lowerMaskL, lowerMaskH)
     higherMask = cv2.inRange(hsvImage, higherMaskL, higherMaskH)
@@ -164,12 +182,12 @@ def circleOnImage(dataDict):
     falseCircles = 0;
     for data in dataDict:
         path = data["path"] + "\/images\/" + data["fileName"]
-        print("path: ", path)
+        data["partDictionaries"].clear()
+
         circles4 = None
         circles5 = None
         image = cv2.imread(path)
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
         bluredImage1 = makingMaskForCircles(hsv, np.array([0, 120, 10]), np.array([10, 255, 255]),
                                             np.array([160, 80, 10]), np.array([180, 255, 255]))
         circles3 = cv2.HoughCircles(bluredImage1, cv2.HOUGH_GRADIENT, 1, 100,
@@ -197,17 +215,27 @@ def circleOnImage(dataDict):
                 cv2.circle(image, (i[0], i[1]), i[2], (0, 255, 0), 2)
                 # draw the center of the circle
                 cv2.circle(image, (i[0], i[1]), 2, (0, 255, 0), 3)
-                xmin = i[0] - i[2] - 10
-                xmax = i[0] + i[2] + 10
-                ymin = i[1] - i[2] - 10
-                ymax = i[1] + i[2] + 10
+                xmin = i[0] - i[2] - 5
+                xmax = i[0] + i[2] + 5
+                ymin = i[1] - i[2] - 5
+                ymax = i[1] + i[2] + 5
+                print(xmin, xmax, ymin, ymax)
+                if(xmax - xmin > int(data["width"])/10 and  ymax - ymin > int(data["height"])/10):
+                    print("IM HERE")
+                    bndboxDict = {
+                        "xmin": xmin,
+                        "ymin": ymin,
+                        "xmax": xmax,
+                        "ymax": ymax}
+                    data["partDictionaries"].update({"bndboxDict":bndboxDict})
         except:
             falseCircles += 1
-
+        data.update({"elementsOnImage" : len(data["partDictionaries"])}) #ilosc elementow wykrytych poprzez algorytm
+        #print("length of dict: ",len(data['partDictionaries']))
         # cv2.imshow(f"images{path_}", np.hstack([image]))
         # cv2.waitKey(0)
     print(falseCircles)
-
+    return dataDict
 
 def main():
     # pathsWithSpeedSights, pathsWithOther = loadingImage()
@@ -221,16 +249,28 @@ def main():
     dataTrain = extract(dataTrain)  # dictionary with added descriptor parameters
     print("traning data")
     afterTrain = train(dataTrain)
+    # print("making dictionary for Test Data")
+    # dataTest = makingDictionaryForLearning("test")
+    # print("extract data Test")
+    # dataTest = extract(dataTest)
+    # print("predict Image")
+    # predictImage(afterTrain, dataTest)
+    # print("calculate Accuracy")
+    # accuracyCalculate(dataTest)
+
+
+    #printingTestImages(dataTest)
+
     print("making dictionary for Test Data")
     dataTest = makingDictionaryForLearning("test")
+    print("searching for interesting places on image")
+    dataTest = circleOnImage(dataTest)
     print("extract data Test")
     dataTest = extract(dataTest)
     print("predict Image")
     predictImage(afterTrain, dataTest)
-    print("calculate Accuracy")
-    accuracyCalculate(dataTest)
-
-    # printingTestImages(dataTest)
+    print("printing choosen pants")
+    printingChoosenparts(dataTest)
 
     # dataTest = makingDictionaryForLearning("test")
     # circleOnImage(dataTest)
